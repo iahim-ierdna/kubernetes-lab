e0e909239c2f2a4337f9d5dc705714936c4cc0b8
Andrei Mihai Iulian <andrei@aimcorp.co.uk>
2022-09-17 16:06:24 +0200

    Create kubeDeploy.sh

---
+++ kubeDeploy.sh
@@ -0,0 +1,59 @@
+#note I already had swap deployed. E.G.: sed -i s/"\/swapfile"/"#\/swapfile"/ /etc/fstab
+
+#run as root.
+
+firewall-cmd --permanent --zone=trusted --add-port=6443/tcp 
+firewall-cmd --permanent --zone=trusted --add-port=2379-2380/tcp
+firewall-cmd --permanent --zone=trusted --add-port=10251/tcp
+firewall-cmd --permanent --zone=trusted --add-port=10252/tcp
+firewall-cmd --permanent --zone=trusted --add-port=10250/tcp
+firewall-cmd --permanent --zone=trusted --add-port=30000-32767/tcp
+firewall-cmd --permanent --zone=trusted --add-port=80/tcp
+firewall-cmd --permanent --zone=trusted --add-port=443/tcp
+firewall-cmd --permanent --zone=trusted --add-port=22/tcp
+firewall-cmd --reload
+firewall-cmd --list-all
+modprobe overlay
+modprobe br_netfilter
+cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+cat << EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+sudo apt update
+sudo apt install containerd
+mkdir  -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sed -i s/SystemdCgroup = false/SystemdCgroup = true/ /etc/containerd/config.toml
+systemctl restart containerd
+
+#the following two lines are based on the official K8s native package manager installation documentation.
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+apt update
+apt-cache policy kubelet | head -n 20 
+#^this is not necessary. We're actually using the command subsitution below, to get the second to last version
+#note that the author of the course performs this in order to demonstrate a k8s upgrade later on.
+#I will post my own upgrade script later on, as I have I made for work.
+
+VERSION=$(apt-cache policy kubelet | head -n 7 | tail -n 1 | cut -d' ' -f 6)
+apt install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
+
+apt-mark hold kubelet kubeadm kubectl containerd
+systemctl enable --now containerd kubelet
+
+#don't go past this point if you're on a worker node.
+
+kubeadm init
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+kubectl get nodes
+
+kubectl get pods -A
