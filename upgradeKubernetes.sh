49888c7ceed954edc648bcd5a39eaf7275b0a16d
Andrei Mihai Iulian <andrei@aimcorp.co.uk>
2023-02-17 12:41:13 +0100

    Create upgradeKubernetes.sh
    This is an in-place upgrade script for Kubernetes Nodes, from version 1.23 to version 1.26. Control Plane and worker nodes can be upgrade.

---
+++ upgradeKubernetes.sh
@@ -0,0 +1,27 @@
+for ver in 4 5 6
+do
+
+VERSION=$(yum list --showduplicates kubeadm --disableexcludes=kubernetes -y | grep 1.2$ver| tail -n 1 | head -n 1 | cut -d' ' -f24)
+sudo yum install -y kubeadm-$VERSION --disableexcludes=kubernetes
+
+if [ ! -z "$(kubectl get node $(hostname) | grep control-plane)" ]
+then
+sudo kubeadm version
+sudo kubeadm upgrade plan
+sudo kubeadm upgrade apply v$VERSION
+else
+sudo kubeadm upgrade node
+fi
+
+kubectl drain $(hostname) --ignore-daemonsets &
+sudo yum install -y kubelet-$VERSION kubectl-$VERSION --disableexcludes=kubernetes
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
+sudo kubeadm init phase kubelet-start
+kubectl uncordon $(hostname)
+
+kubectl get nodes
+
+read
+
+done
