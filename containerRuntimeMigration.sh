10a6167c0ecd401623379c23a5dba3c3b86391ff
Andrei Mihai Iulian <andrei@aimcorp.co.uk>
2023-04-11 15:20:21 +0200

    Update containerRuntimeMigration.sh

--- containerRuntimeMigration.sh
+++ containerRuntimeMigration.sh
@@ -1,48 +1,35 @@
 #base article here https://kubernetes.io/docs/tasks/administer-cluster/migrating-from-dockershim/change-runtime-containerd/
 
-kubectl drain $(hostname) --ignore-daemonsets &
-
+kubectl drain $(hostname | tr '[:upper:]' '[:lower:]') --ignore-daemonsets &
 echo waiting 30 seconds for node $(hostname) to be drained...
-
 sleep 30
-
 sudo systemctl stop kubelet
-
 sudo systemctl disable docker --now
-sudo yum remove docker-ce docker-ce-cli -y 
+sudo yum remove docker-ce docker-ce-cli -y
 sudo modprobe overlay
-
 sudo modprobe br_netfilter
-
 sudo cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-
 sudo cat << EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
-
 sudo sysctl --system
-
 sudo yum install containerd -y
-
 sudo mkdir  -p /etc/containerd
-
 containerd config default | sudo tee /etc/containerd/config.toml
-
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-
+sudo sed -i 's/disabled_plugins.*/#disabled_plugins/' /etc/containerd/config.toml
 sudo systemctl restart containerd
-
 sudo sed -i.bak 's/KUBELET_KUBEADM_ARGS=\".*/KUBELET_KUBEADM_ARGS=\"--container-runtime=remote  --container-runtime-endpoint=unix:\/\/\/run\/containerd\/containerd.sock\"/' /var/lib/kubelet/kubeadm-flags.env
 
-kubectl patch no $(hostname) --patch '{"metadata": {"annotations": {"kubeadm.alpha.kubernetes.io/cri-socket": "unix:///run/containerd/containerd.sock"}}}'
+kubectl patch no  $(hostname | tr '[:upper:]' '[:lower:]') --patch '{"metadata": {"annotations": {"kubeadm.alpha.kubernetes.io/cri-socket": "unix:///run/containerd/containerd.sock"}}}'
+
 sudo systemctl start kubelet
 
-kubectl uncordon $(hostname)
+kubectl uncordon  $(hostname | tr '[:upper:]' '[:lower:]')
 
 kubectl get nodes -o wide
-
