c7f2e5f637c47963b72006e99af35b3f9b39e9a9
Andrei Mihai Iulian <andrei@aimcorp.co.uk>
2023-04-11 13:46:59 +0200

    Update upgradeKubernetes.sh

--- upgradeKubernetes.sh
+++ upgradeKubernetes.sh
@@ -1,4 +1,5 @@
 # based on this https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/kubeadm-upgrade/
+sudo sed -i.bak 's/exclude=kube\*/#exclude=kube\*/' /etc/yum.repos.d/kubernetes.repo
 
 for ver in 4 5 6
 do
@@ -12,24 +13,34 @@ else
 sudo yum install -y kubeadm-$VERSION --disableexcludes=kubernetes
 fi
 
-if [ ! -z "$(kubectl get node $(hostname) | grep control-plane)" ]
+if [ ! -z "$(kubectl get node $(hostname | tr '[:upper:]' '[:lower:]') | grep control-plane)" ]
+
 then
-sudo kubeadm version
-sudo kubeadm upgrade plan
-sudo kubeadm upgrade apply v$VERSION
+echo Upgrading Control Plane Node!
+read -p "Press any key to continue:"
+
+        sudo kubeadm version
+
+        sudo kubeadm upgrade plan
+
+        read -p "Please review the upgrade plan details above and press a key to continue if you agree it's safe."
+        
+        sudo kubeadm upgrade apply v$VERSION
 else
-sudo kubeadm upgrade node
+
+        sudo kubeadm upgrade node
 fi
 
-kubectl drain $(hostname) --ignore-daemonsets &
+kubectl drain $(hostname | tr '[:upper:]' '[:lower:]') --ignore-daemonsets
 sudo yum install -y kubelet-$VERSION kubectl-$VERSION --disableexcludes=kubernetes
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 sudo kubeadm init phase kubelet-start
-kubectl uncordon $(hostname)
+kubectl uncordon  $(hostname | tr '[:upper:]' '[:lower:]')
 
 kubectl get nodes
 
-read -p "Press any key to continue:"
+read -p "Please review the previou upgrade step, make sure all notes are on $VERSION, then press any key to continue!"
 
 done
+sudo sed -i.bak 's/#exclude=kube\*/exclude=kube\*/' /etc/yum.repos.d/kubernetes.repo
