a4e50c68f4688041f4654e255c125c9fa2968117
Andrei Mihai Iulian <andrei@aimcorp.co.uk>
2023-04-11 13:42:39 +0200

    Update upgradeKubernetes.sh

--- upgradeKubernetes.sh
+++ upgradeKubernetes.sh
@@ -4,7 +4,13 @@ for ver in 4 5 6
 do
 
 VERSION=$(yum list --showduplicates kubeadm --disableexcludes=kubernetes -y | grep 1.2$ver| tail -n 1 | head -n 1 | cut -d' ' -f24)
+if [ "$(echo kubeadm-$VERSION)" == "$(rpm -qa | grep kubeadm | cut -d'.' -f1-3)" ]
+then
+echo kubeadm-$VERSION is already installed. Skipping rope...
+continue
+else
 sudo yum install -y kubeadm-$VERSION --disableexcludes=kubernetes
+fi
 
 if [ ! -z "$(kubectl get node $(hostname) | grep control-plane)" ]
 then
